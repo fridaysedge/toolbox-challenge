11ec11c2a9c3743b3ac599c2b7d7214acf0b902f
Chris Sumption <fridaysedge@gmail.com>
2014-10-27 12:22:09 -0700

    status at the end of class

--- js/app.js
+++ js/app.js
@@ -22,4 +22,52 @@ $(document).ready(function(){
         tilePairs.push(_.clone(tile));
     });
 
+    tilePairs = _.shuffle(tilePairs);
+
+    console.log(tilePairs);
+
+    var gameBoard = $('#game-board');
+    var row = $(document.createElement('div'));
+    var img;
+    _.forEach(tilePairs, function(tile, elemIndex){
+
+        if(elemIndex > 0 && 0 == elemIndex %4){
+            gameBoard.append(row);
+            row = $(document.createElement('div'));
+        };
+       img = $(document.createElement('img'));
+        img.attr({
+            src: 'img/tile-back.png',
+            alt: 'image of tile ' + tile.tileNum
+        });
+        img.data('tile', tile);
+        row.append(img);
+    });
+
+    gameBoard.append(row);
+
+    $('#game-board img').click(function(){
+       var img = $(this);
+       var tile = img.data('tile');
+       img.fadeOut(100, function(){
+            if(tile.flipped){
+                img.attr('src', 'img/tile-back.png');
+            }else{
+                img.attr('src', tile.src);
+            }
+            tile.flipped = !tile.flipped;
+            img.fadeIn(100);
+        });
+    });
+    var startTime = _.now();
+    var timer = window.setInterval(function(){
+        var elapsedSeconds = Math.floor((_.now() - startTime) / 1000);
+        $('#elapsed-seconds').text(elapsedSeconds);
+
+        if (elapsedSeconds > 10) {
+            window.clearInterval(timer);
+        }
+
+    }, 1000);
+
 });
